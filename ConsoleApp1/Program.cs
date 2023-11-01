eca97feb9b251fea892c015eadbeaeea71385dbb
S07C09 <S07C09@S07C09>
2023-11-01 08:15:47 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿class program
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int a = int.Parse(Console.ReadLine());
+        switch (a)
+        {
+            case 1:
+                Console.WriteLine("One");
+                break;
+            case 2:
+                Console.WriteLine("Two");
+                break;
+            case 3:
+                Console.WriteLine("Three");
+                break;
+            default: Console.WriteLine("Error"); break;
+        }
+        Console.ReadKey();
+    }
+}
